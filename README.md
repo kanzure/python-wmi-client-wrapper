f4097a6f21d4ba81321f5674cd3cd5dda1183ea0
Bryan Bishop <kanzure@gmail.com>
2013-08-19 13:33:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# python-wmi-client-wrapper
+
+This is a wrapper around wmi-client for Linux. Apparently the python-wmi module
+uses Windows APIs to access WMI, which isn't something that is going to work on
+Linux.
+
+## installing
+
+```
+pip install wmi-client-wrapper
+```
+
+## usage
+
+```
+import wmi_client_wrapper as wmi
+output = wmi.query("Win32_Processor")
+```
+
+## testing
+
+```
+nosetests
+```
+
+## license
+
+BSD
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# for uploading to pypi
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+# There's some intersection with requirements.txt but pypi can't handle git
+# dependencies. So it's better to just manually list dependencies again.
+requires = [
+    "mock",
+    "sh",
+]
+
+setup(
+    name="wmi-client-wrapper",
+    version="0.0.0",
+    description="Linux-only wrapper around wmi-client for WMI (Windows)",
+    long_description=open("README.md", "r").read(),
+    license="BSD",
+    author="Bryan Bishop",
+    author_email="kanzure@gmail.com",
+    url="https://github.com/kanzure/python-wmi-client-wrapper",
+    packages=["wmi_client_wrapper"],
+    package_dir={"wmi_client_wrapper": "wmi_client_wrapper"},
+    include_package_data=True,
+    install_requires=requires,
+    platforms="any",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Linux",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
