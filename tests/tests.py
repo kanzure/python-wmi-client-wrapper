909fc341fc882758691e926fafdcdb99bb800d0f
Bryan Bishop <kanzure@gmail.com>
2013-08-19 14:02:00 -0500

    fix test definition to accept self

--- tests/tests.py
+++ tests/tests.py
@@ -4,8 +4,8 @@ import mock
 import wmi_client_wrapper as wmi
 
 class TestCases(unittest.TestCase):
-    def test_object_creation():
-        wmic = wmi()
+    def test_object_creation(self):
+        wmic = wmi.WmiClientWrapper()
 
 if __name__ == "__main__":
     unittest.main()
