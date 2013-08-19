04cc6b2237f05bbd5f48d72604a22d83b170968b
Bryan Bishop <kanzure@gmail.com>
2013-08-19 14:04:18 -0500

    some tests for the constructor

--- tests/tests.py
+++ tests/tests.py
@@ -4,8 +4,20 @@ import mock
 import wmi_client_wrapper as wmi
 
 class TestCases(unittest.TestCase):
+    def test_object_creation_raises_without_username(self):
+        with self.assertRaises(Exception):
+            wmic = wmi.WmiClientWrapper()
+
+    def test_object_creation_raises_without_password(self):
+        with self.assertRaises(Exception):
+            wmic = wmi.WmiClientWrapper(username="Administrator")
+
+    def test_object_creation_raises_without_host(self):
+        with self.assertRaises(Exception):
+            wmic = wmi.WmiClientWrapper(username="Administrator", password="password")
+
     def test_object_creation(self):
-        wmic = wmi.WmiClientWrapper()
+        wmic = wmi.WmiClientWrapper(username="Administrator", password="password", host="192.168.1.173")
 
 if __name__ == "__main__":
     unittest.main()
