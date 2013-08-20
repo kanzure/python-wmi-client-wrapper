000504577ca03c909d11f3ea522879e2c26053b6
Bryan Bishop <kanzure@gmail.com>
2013-08-19 19:37:51 -0500

    version bump to: v0.0.4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requires = [
 
 setup(
     name="wmi-client-wrapper",
-    version="0.0.3",
+    version="0.0.4",
     description="Linux-only wrapper around wmi-client for WMI (Windows)",
     long_description=open("README.md", "r").read(),
     license="BSD",
